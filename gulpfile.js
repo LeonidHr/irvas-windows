1ab36532be57d4da3bd392c79a1de5a09e3f99d5
Leonid <hristenokleonid@gmail.com>
2026-09-08 17:44:43 +0300

    Add forms and calc validation dist

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-// const dist = "./dist/";
-const dist = "D:\\OSPanel\\home\\windows\\";
+const dist = "./dist/";
+// const dist = "D:\\OSPanel\\home\\windows\\";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
